5366f39327fa3fb0bc5706017a7ef0f67edc84d6
yhj <nove7@qq.com>
2026-06-07 23:19:56 +0800

    basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Editor from "@/pages/Editor";
-const router = createBrowserRouter([{ path: "/", element: <Editor /> }]);
+const router = createBrowserRouter([{ path: "/", element: <Editor /> }], {
+  basename: "/special-resume",
+});
 
 export default router;
